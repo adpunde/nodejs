66b97d4afa3a630d3667bfda1e898d0c46b46381
Avadhoot Punde <avadhoot.punde@gmail.com>
2017-04-23 17:09:07 +0530

    Default app  generation using express generator

---
+++ 7-mongodb/connection.js
@@ -0,0 +1,26 @@
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+
+var dbUrl = 'mongodb://localhost:27017/newDB';
+
+MongoClient.connect(dbUrl, function(err, db) {
+    assert.equal(err, null);
+    console.log('Connected to MongoDB');
+
+    var collection = db.collection('collection');
+
+    collection.insertOne({name: 'Name', description: 'Test'}, function(err, result) {
+        assert.equal(err, null);
+        console.log(result.ops);
+
+        collection.find({}).toArray(function(err, docs) {
+            assert.equal(err, null);
+            console.log("Found:", docs);
+
+            db.dropCollection('collection', function(err, result) {
+                assert.equal(err, null);
+                db.close();
+            });
+        });
+    });
+});
